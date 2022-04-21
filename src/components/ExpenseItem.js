711dfb391d5aeca6f9aed90fb559b72852dc0e28
Alexander Bravo Martinez <abravo@pros.com>
2022-04-20 21:09:48 -0500

    setting up our first hook 'useState'
    * Adding a button example just to experiment with useState Hook

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,28 @@
+// We need to import a named function useState.
+// We use useState when need to handler acction from the user UI.
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+  //lerning about useState Hook.
+  // 1. We define our statae passing the property we want to handle.
+  // this can be an object too.
+  const [title, setTitle] = useState(props.title);
+  //2. Creates a handler functions,
+  // this function can not be aoutside the react component
+  const titleHandler = () => {
+    setTitle("Update!");
+    console.log(title);
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
         <div className="expense-item__price">{props.amount}</div>
+        <button onClick={titleHandler}>Update</button>
       </div>
     </div>
   );
