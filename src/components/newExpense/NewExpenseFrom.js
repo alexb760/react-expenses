6feea8f42903b9e397cd700ede1d2cbc8ddb6e09
Alexander Bravo Martinez <abravo@pros.com>
2022-05-05 21:40:53 -0500

    handling useState data
    * learning how to interact with forms data in React by using useStare hook

--- src/components/newExpense/NewExpenseFrom.js
+++ src/components/newExpense/NewExpenseFrom.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 const NewExpenseForm = () => {
+  const [userInput, setUserInput] = useState({
+    titleInput: "",
+    amoundInput: "",
+    dateInput: "",
+  });
+
+  const titleChangeHandler = (event) => {
+    // Keep in mind this approach is no recomended, it will work most of the cases though.
+    // But since React schedule state changes it won't garante that you will get latest snapshot of the state.
+    // Opcion 1: no recomended.
+    // -> setUserInput({ ...userInput, titleInput: event.target.value });
+    // Option 2: recomended
+    // React will garante you will get latest snapshot of the state.
+    setUserInput((prevState) => {
+      return { ...prevState, titleInput: event.target.value };
+    });
+    //Option 3: is handle individual state for each input declarin separatly.
+  };
+
+  const amaountChangeHandler = (event) => {
+    setUserInput({ ...userInput, amoundInput: event.target.value });
+  };
+
+  const dateChangeHandler = (event) => {
+    setUserInput({ ...userInput, dateInput: event.target.value });
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
